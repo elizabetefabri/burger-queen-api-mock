9703304db9b3567124a3c9580d33ed6a909a5553
Elizabete Fabri <elizabete958@gmail.com>
2025-08-31 16:24:24 -0300

    ajuste no router e rules

--- api/server.js
+++ api/server.js
@@ -4,12 +4,10 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 
 const app = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "..", "db.json"));
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-const rules = auth.rewriter(
-  JSON.parse(fs.readFileSync(path.join(__dirname, "..", "routes.json")))
-);
+const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
 
 app.db = router.db;
 app.use(middlewares);
